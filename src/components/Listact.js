46032efa10452c58e958f1aadbd221859e08563c
Gaston <gastoncorrea90e@gmail.com>
2021-05-22 23:04:57 -0300

    Se termino un pequeño imput para poder agregar una categoria nueva

--- src/components/Listact.js
+++ src/components/Listact.js
@@ -1,12 +1,14 @@
+import { Button } from "react-bootstrap";
 import React from "react";
-import { Container, ListGroup } from "react-bootstrap";
+import { Container, Form, ListGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Listact = () => {
   return (
     <Container className="my-5 py-5">
       <h1 className="display-1">
-        Elija una categoria para poder ver las noticias
+        Elija una categoria para poder ver las noticias o ingrese una nueva
+        categoria
       </h1>
       <ListGroup as="ul" className="my-5">
         <ListGroup.Item as="li" active>
@@ -82,6 +84,16 @@ const Listact = () => {
           </Link>
         </ListGroup.Item>
       </ListGroup>
+      <Form>
+        <Form.Group controlId="agregar-categoria" className="my-3">
+          <Form.Label>Agregar una nueva Categoria</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Si desea ingrese una nueva categoria"
+          />
+        </Form.Group>
+        <Button className="btn btn-primary">Guardar</Button>
+      </Form>
     </Container>
   );
 };
