eb84de5a44d474b1612b60cb6c411d04e619fc3b
Gaston <gastoncorrea90e@gmail.com>
2021-05-22 23:30:50 -0300

    Se agrego fecha de nacimiento al form de suscripcion

--- src/components/Suscripcion.js
+++ src/components/Suscripcion.js
@@ -6,8 +6,8 @@ const Suscripcion = () => {
   return (
     <Fragment>
       <Container>
-        <section className='my-5 pt-5'>
-          <div className='my-5'>
+        <section className="my-5 pt-5">
+          <div className="my-5">
             <h1 className="display-1">
               Suscribite a la pagina de noticias y disfruta de maravillosos
               beneficios
@@ -18,21 +18,28 @@ const Suscripcion = () => {
               <Form.Label>Nombre y Apellido</Form.Label>
               <Form.Control type="text" placeholder="Gaston Correa" />
             </Form.Group>
-            <Form.Group controlId="email-suscripcion" className='my-3' >
+            <Form.Group controlId="fecha-nacimiento" className='my-3'>
+              <Form.Label>Fecha de nacimiento</Form.Label>
+              <Form.Control type="date" placeholder="DD-MM-AAAA" />
+            </Form.Group>
+            <Form.Group controlId="email-suscripcion" className="my-3">
               <Form.Label>Correo electronico</Form.Label>
               <Form.Control type="email" placeholder="gaston@correa.com" />
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
             </Form.Group>
-            <Form.Group controlId="contraseña-suscripcion" className='my-3'>
+            <Form.Group controlId="contraseña-suscripcion" className="my-3">
               <Form.Label>Contraseña</Form.Label>
               <Form.Control type="password" placeholder="Password" />
             </Form.Group>
             <Form.Group controlId="check-termino">
-              <Form.Check type="checkbox" label="Acepto los terminos y condiciones" />
+              <Form.Check
+                type="checkbox"
+                label="Acepto los terminos y condiciones"
+              />
             </Form.Group>
-            <Button variant="primary" type="submit" className='mt-3'>
+            <Button variant="primary" type="submit" className="mt-3">
               Suscribirme
             </Button>
           </Form>
